6eda9dfd3160e29762286c22c7a2f923fc687512
Jamie Bryson <jamieab98@gmail.com>
2025-11-04 16:31:36 -0500

    Finished in one go again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,37 @@
 // create your App component here
+import { useEffect, useState } from "react";
+const dogURL = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+    const [dogImage, setDogImage] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        newDog()
+    }, []);
+
+    function newDog() {
+        setLoading(true);
+        fetch(dogURL)
+        .then(response => {
+            if (!response.ok) {console.log("Failed to fetch Image");}
+            return response.json();
+        })
+        .then(data => {
+            setDogImage(data.message);
+            setLoading(false);
+        })
+        .catch(error => console.log(error));
+    }
+    
+    return(
+        <>
+            <button onClick={newDog}>Click for New Dog</button>
+            <div>
+                {loading ? <p>Loading...</p> : <img src={dogImage}></img>}
+            </div>
+        </>
+    )
+}
+
+export default App
